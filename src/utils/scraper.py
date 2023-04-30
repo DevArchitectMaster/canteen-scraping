bd82cdf428d8f7b1003685aa45919dec07370f64
DevArchitectMaster <developer@felix-boeck.de>
2023-05-01 01:22:20 +0200

    refactoring scraper

--- src/utils/scraper.py
+++ src/utils/scraper.py
@@ -1,7 +1,7 @@
 import enum
 
+import urllib
 import requests
-from urllib.request import urlopen
 
 NotImplementedErrorMessage = "\n\n\t\t\t### the selected scraper library is not available yet ###\t\t\n\t\t### please select an existing one from the enum '__SCRAPER_LIB' instead ###\t\t\n\n"
 
@@ -10,20 +10,21 @@ class Scraper:
         URLLIB = "urllib"
         REQUESTS = "requests"
 
-    def __init__(self, url=None, decoding="utf-8", scraper=__SCRAPER_LIB.REQUESTS, **kwargs):
-        self.__scraper = scraper
+    def __init__(self, url=None, encoding="utf-8", scraper=__SCRAPER_LIB.REQUESTS, **kwargs):
+        self.__scraper_lib = scraper
+        self.__scraper = None
         self.__url = url
-        self.__decoding = decoding
+        self.__encoding = encoding
         self.__html = None
 
     def __del__(self):
         pass
     
     def getScraper(self):
-        return self.__scraper
+        return self.__scraper_lib
     
-    def getDecoding(self):
-        return self.__decoding
+    def getEncoding(self):
+        return self.__encoding
     
     def getUrl(self):
         return self.__url
@@ -31,17 +32,17 @@ class Scraper:
     def getHtml(self):
         return self.__html
     
-    def openurl(self, url=None, decoding=None):
+    def openurl(self, url=None, encoding=None):
         if url is not None:
             self.__url = url
-        if decoding is not None:
-            self.__decoding = decoding
+        if encoding is not None:
+            self.__encoding = encoding
 
         # selection of the scraper lib
-        if self.__scraper == self.__SCRAPER_LIB.URLLIB:
-            self.__html = self.__getHtmlByUrllib(url, decoding)
-        elif self.__scraper == self.__SCRAPER_LIB.REQUESTS:
-            self.__html = self.__getHtmlByRequest(url, decoding)
+        if self.__scraper_lib == self.__SCRAPER_LIB.URLLIB:
+            self.__html = self.__getHtmlByUrllib(self.__url, self.__encoding)
+        elif self.__scraper_lib == self.__SCRAPER_LIB.REQUESTS:
+            self.__html = self.__getHtmlByRequest(self.__url, self.__encoding)
         else:
             raise NotImplementedError(NotImplementedErrorMessage)
             #exit()
@@ -55,11 +56,13 @@ class Scraper:
     #                                                        urllib methods                                                          #
     #                                                                                                                                #
     ##################################################################################################################################
+
+    # docs.python.org/3/library/urllib
     
-    def __getHtmlByUrllib(self, url, decoding):
-        page = urlopen(url)
+    def __getHtmlByUrllib(self, url, encoding):
+        page = urllib.request.urlopen(url)
         html_bytes = page.read()
-        html = html_bytes.decode(decoding)
+        html = html_bytes.decode(encoding)
         return html
     
     ##################################################################################################################################
@@ -67,9 +70,12 @@ class Scraper:
     #                                                      requests methods                                                          #
     #                                                                                                                                #
     ##################################################################################################################################
+
+    # requests.readthedocs.io
     
-    def __getHtmlByRequest(self, url, decoding):
+    def __getHtmlByRequest(self, url, encoding):
         response = requests.get(url)
+        response.encoding = encoding
         if response.status_code != 200:
             print("Error fetching page")
             exit()
