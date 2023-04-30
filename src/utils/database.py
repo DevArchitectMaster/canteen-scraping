02c3dc90d3c1f1f66fb9b252569c2f0ae6c99d33
DevArchitectMaster <developer@felix-boeck.de>
2023-04-30 18:03:29 +0200

    update database

--- src/utils/database.py
+++ src/utils/database.py
@@ -4,23 +4,25 @@ import sqlite3
 NotImplementedErrorMessage = "\n\n\t\t\t### the CRUD operations are not implemented ###\t\t\n\t\t### please use 'read_data()' and 'write_data()' instead ###\t\t\n\n"
 
 class SQLite():
-    def __init__(self, database, **kwargs):
-        self.__database = database
+    def __init__(self, database=None, **kwargs):
+        self.__database = self.__check_database(database)
         self.__connection = self.connect(self.__database)
         self.__cursor = self.__connection.cursor()
 
     def __del__(self):
         self.close()
 
+    def __check_database(self, database=None):
+        if database is None:
+            database = ":memory:"
+        return database
+
     def connect(self, database=None):
         """ create a database connection to the SQLite database specified by db_file
         :param db_file: database file
         :return: Connection object or None
         """
-        if database is None:
-            database = self.__database
-        else:
-            self.__database = database
+        self.__database = self.__check_database(database)
         
         try:
             self.__connection = sqlite3.connect(database)
@@ -31,6 +33,8 @@ class SQLite():
         return self.__connection
 
     def close(self):
+        #TODO:
+        #self.__cursor.close()
         self.__connection.close()
 
     def read_sql_file(self, file, mode='r', encoding='utf-8'):
@@ -48,9 +52,17 @@ class SQLite():
     
     ##################################################################################################################################
     
-    def read_data(self, sql_statement):
+    def read_data(self, sql_statement, columns=False):
         self.__cursor.execute(sql_statement)
-        return self.__cursor.fetchall()
+
+        if columns is True:
+            # https://peps.python.org/pep-0249/#cursor-attributes
+            cols_name = [cursor_attributes[0] for cursor_attributes in self.__cursor.description]
+            results = [dict(zip(cols_name, item)) for item in self.__cursor.fetchall()]
+        else:
+            results = self.__cursor.fetchall()
+
+        return results
     
     def write_data(self, sql_statement, values=None):
         if values is None:
@@ -93,9 +105,9 @@ class SQLite():
         self.truncate_database(self.__connection)
         self.write_data(sql_statement=init_statement)
     
-    def readDataSetById(self, id):
+    def readDataSetById(self, id, columns=False):
         sql_statement = ''' SELECT * FROM results WHERE id=''' + str(id) + ''' '''
-        return self.read_data(sql_statement=sql_statement)
+        return self.read_data(sql_statement=sql_statement, columns=columns)
     
     def write_scraped_line(self, values):
         sql_statement = ''' INSERT INTO results(scrapling_timestamp, intended_date, courses_type, label, ingredients, icons, price_students, price_staff, price_guests, price_special_fare) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?) '''
