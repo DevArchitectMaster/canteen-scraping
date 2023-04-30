036cc362dcfd4038f8b893260dd4bf4ce064e041
DevArchitectMaster <developer@felix-boeck.de>
2023-04-30 14:40:04 +0200

    update database

--- src/utils/database.py
+++ src/utils/database.py
@@ -2,20 +2,22 @@
 import sqlite3
 
 class SQLite():
-    def __init__(self, db, **kwargs):
-        self.__database = db
+    def __init__(self, database, **kwargs):
+        self.__database = database
         self.__connection = self.connect(self.__database)
-        
-    def connect(self, db=None):
+
+    def connect(self, database=None):
         """ create a database connection to the SQLite database specified by db_file
         :param db_file: database file
         :return: Connection object or None
         """
-        if db is None:
-            db = self.__database
+        if database is None:
+            database = self.__database
+        else:
+            self.__database = database
         
         try:
-            self.__connection = sqlite3.connect(db)
+            self.__connection = sqlite3.connect(database)
         except sqlite3.Error as e:
             print(e)
 
@@ -23,6 +25,11 @@ class SQLite():
     
     def close(self):
         self.__connection.close()
+
+    def __setConnection(self, connection=None):
+        if connection is None:
+            connection = self.__connection
+        return connection
 
     def read_sql_file(self, file, mode='r', encoding='utf-8'):
         file = open(file, mode, encoding=encoding)
@@ -33,28 +40,22 @@ class SQLite():
         return file_content
 
     def execute(self, execution_statement, connection=None):
-        if connection is None:
-              connection = self.__connection
+        db_connection = self.__setConnection(connection=connection)
         
-        db_cursor = connection.cursor()
+        db_cursor = db_connection.cursor()
         return db_cursor.execute(execution_statement)
     
     def write_data(self, sql_statement, values=None, connection=None):
-        if connection is None:
-              connection = self.__connection
+        db_connection = self.__setConnection(connection=connection)
         
-        db_cursor = connection.cursor()
-        if connection is None:
+        db_cursor = db_connection.cursor()
+        if values is None:
             db_cursor.execute(sql_statement)
         else:
             db_cursor.execute(sql_statement, values)
-        connection.commit()
-        #results = connection.fetchall()
+        db_connection.commit()
+        #results = db_connection.fetchall()
         return db_cursor.lastrowid
-    
-    def truncate_database(self, connection=None):
-        truncate_statement = self.read_sql_file("./src/backup/database/truncate_database.sql")
-        return self.execute(truncate_statement)
     
     ##################################################################################################################################
     #                                                                                                                                #
@@ -62,11 +63,22 @@ class SQLite():
     #                                                                                                                                #
     ##################################################################################################################################
 
+    def truncate_database(self, connection=None):
+        truncate_statement = self.read_sql_file("./src/backup/database/truncate_database.sql")
+        return self.execute(execution_statement=truncate_statement, connection=connection)
+
     def init_database(self, connection=None):
         init_statement = self.read_sql_file("./src/backup/database/create_tables.sql")
-        self.truncate_database()
-        return self.execute(init_statement)
+        self.truncate_database(connection)
+        return self.execute(execution_statement=init_statement, connection=connection)
+    
+    def readDataSetById(self, id, connection=None):
+        sql_statement = ''' SELECT * FROM results WHERE id=''' + str(id) + ''' '''
+        #return self.execute(execution_statement=sql_statement, connection=connection)
+        temp = self.execute(execution_statement=sql_statement, connection=connection)
+        print(temp)
+        return temp
     
     def write_scraped_line(self, values, connection=None):
         sql_statement = ''' INSERT INTO results(scrapling_timestamp, intended_date, courses_type, label, ingredients, icons, price_students, price_staff, price_guests, price_special_fare) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?) '''
-        return self.write_data(sql_statement, values)
+        return self.write_data(sql_statement=sql_statement, values=values, connection=connection)
